eeca801d2666ebbfefe46a6149749ad8e0cdcffd
sonia chavepeyre <ciarasonia013@gmail.com>
2024-06-18 15:11:07 +0200

    controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,35 +1,3 @@
 class EventsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index, :show ]
-  def index
-    @events = Event.all
-  end
 
-  def show
-    @event = Event.find(params[:id])
-  end
-
-  def new
-    @event = Event.new
-  end
-
-  def create
-    @event = Event.new(event_params)
-    @event.user = current_user
-    if @event.save!
-      redirect_to events_path
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    @event.update!(event_params)
-    redirect_to events_path, status: :see_other
-  end
-  private
-
-  def event_params
-    params.require(:event).permit(:title)
-  end
 end
