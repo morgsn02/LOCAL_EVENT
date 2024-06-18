c2560a0cf50c35c52d14bb18a9571f7fd0d65b25
mohcine chnahim <chnahim_6@hotmail.com>
2024-06-18 15:33:32 +0200

    userscolumn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_18_084004) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_18_133116) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,7 +51,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_18_084004) do
     t.string "last_name"
     t.string "nickname"
     t.string "favorite"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
