f31bf82270d71c9eb64358c99dcc049a2df2d35e
sonia chavepeyre <ciarasonia013@gmail.com>
2024-06-18 15:06:54 +0200

    controller

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,2 +1,42 @@
 class WishlistsController < ApplicationController
+  before_action :set_whishlist, only: [:destroy]
+
+  def show
+  end
+
+  def new
+    @whishlist = Whishlist.new
+  end
+
+  def create
+    @event = Event.find(params[:event_id])
+    @whishlist = Whishlist.new(whislist_params)
+    @whishlist.event = @event
+
+    @whishlist.user = current_user
+    if @whishlist.save
+      redirect_to whishlist_path(@whishlist)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @whishlist.destroy
+    redirect_to whishlist_path, status: :see_other
+  end
+
+  def my_whishlist
+    @whishlists = Whishlist.where(user_id: current_user.id)
+  end
+
+  private
+
+  def whishlist_params
+    params.require(:whislist).permit()
+  end
+
+  def set_whishlist
+    @whishlist = Whishlist.find(params[:id])
+  end
 end
