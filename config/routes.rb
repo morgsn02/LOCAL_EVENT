4def367af54e58dee2c3b0439b031c60ad98eb46
Leanne Fraser <leannejf@gmail.com>
2024-06-18 16:00:53 +0200

    events_controller vue_new vue_home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :events do
+    resources :wishlist, only: [:create, :new]
+  end
+
+  resources :wishlist, only: [:destroy]
+
   root to: "pages#home"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
